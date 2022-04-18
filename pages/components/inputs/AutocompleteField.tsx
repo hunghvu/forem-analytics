3d656131cf7690c4b49a201472703eb4688a0472
hunghvu <hunghvu2017@gmail.com>
2022-04-18 15:39:02 -0700

    feat: Add auto complete component, controlled by react-hook-form

---
+++ pages/components/inputs/AutocompleteField.tsx
@@ -0,0 +1,67 @@
+/**
+ * @author Hung Vu
+ *
+ * This provides an autocomplete field for a given form.
+ */
+
+// React
+import type { FC } from "react";
+
+// Next
+import Image from "next/image";
+
+// MUI library
+import { Autocomplete, Box, TextField } from "@mui/material";
+
+// Utilities
+import { Controller } from "react-hook-form";
+import type { Control } from "react-hook-form";
+
+interface AutocompleteFieldProps {
+  name: string;
+  control: Control<any, any> | undefined;
+  options: string[];
+  label: string;
+  errors: any;
+  iconUrl?: string;
+}
+
+const AutocompleteField: FC<AutocompleteFieldProps> = ({ name, control, options, label, errors, iconUrl }) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={{ required: true }}
+      render={({ field: { onChange, value } }) => (
+        <Autocomplete
+          disablePortal
+          options={options}
+          onChange={(_event, item) => {
+            onChange(item);
+          }}
+          value={value}
+          renderOption={(props, option) =>
+            iconUrl ? (
+              <Box component="li" {...props}>
+                <Image src={iconUrl} width={12} height={12} />
+                {option}
+              </Box>
+            ) : null
+          }
+          renderInput={(params) => (
+            <TextField
+              variant="outlined"
+              {...params}
+              label={label}
+              error={!!errors?.[name]}
+              helperText={errors?.[name] && "Field is required."}
+              required
+            />
+          )}
+        />
+      )}
+    />
+  );
+};
+
+export default AutocompleteField;
