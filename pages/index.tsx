e0ca3ad59a7c6bd351aed0654e0a98b1973f4fd2
hunghvu <hunghvu2017@gmail.com>
2022-04-26 17:40:44 -0700

    chore: Update packages and apply essential tags in <head>

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@
 import { useEffect, useState } from "react";
 
 // Next
+import Head from "next/head";
+
+// Next
 import type { NextPage } from "next";
 
 // MUI library
@@ -26,8 +29,33 @@ const Home: NextPage = (props: any) => {
   useEffect(() => {
     setArticleList(props.articleList);
   }, [props.articleList]);
+  const favicon = "/favicon.png";
+  const title = "Forem Analytics - Stats for Forem communities";
+  const description =
+    "Forem Analytics lets you keep up with the trends across Forem communities by tracking the comment and reaction counts grouped by different metrics.";
+  const url = "https://foremanalytics.sdeproject.com";
+  const socialImage = "/social-image.png";
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="canonical" href={url} />
+        <link rel="icon" type="image/png" href={favicon} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta name="image" property="og:image" content={socialImage} />
+        <link rel="icon" type="image/png" href={favicon} />
+        <meta name="theme-color" content="#f5f5f5" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={socialImage} />
+      </Head>
       <Box
         component="main"
         style={{
